afea52f37d19ec032ad0fb67eff9e006d2607b80
Pablo Antonini <pabloantoninivazquez@gmail.com>
2020-10-19 07:29:51 +0200

    deployed

--- config/dbconnect.js
+++ config/dbconnect.js
@@ -19,6 +19,6 @@ const dbconnect = () =>{
 
     const mongoose = require('mongoose');
     
-    const MONGOUri = process.env.MONGOUri || "mongodb+srv://antonini-333:juventus21@clusterpablo.6opsi.mongodb.net/clinica_dental?retryWrites=true&w=majority";
+    const MONGOUri = process.env.MONGOUri || "mongodb://localhost:27017/pruebas";
     mongoose.connect(MongoURI,{useCreateIndex:true, useNewUrlParser:true, useUnifiedTopology: true})
     .then(()=>console.log('conected to Mongodb:' +MONGOUri))
