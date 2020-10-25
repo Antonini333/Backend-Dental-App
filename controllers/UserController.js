d446f4705e561562fea21fd2990cba2569c112da
Pablo Antonini <pabloantoninivazquez@gmail.com>
2020-10-25 13:27:38 +0100

    fixed controller

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -73,12 +73,13 @@ const UserController = {
                 userFound.token = token;
                 await userFound.replaceOne(userFound);
 
-                res.send(
-                    [name = userFound.name,
-                    email = userFound.email,
-                    token = userFound.token]
+                res.send({
+                    message: `Welcome back ${userFound.name}`,
+                    name: userFound.name,
+                    email: userFound.email,
+                    token: userFound.token
             
-                );
+                });
             }else{
                 return res.status(400).send({
                     message: "Wrong credentials"
